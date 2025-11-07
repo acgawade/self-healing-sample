c6ca0b137f4169d6f85285d4272f0067328fc7f8
acgawade <gawade.achyut96@gmail.com>
2025-11-07 01:28:16 +0000

    committing multiple errors

---
+++ src/main/java/com/vinay/sample/service/HelperService.java
@@ -0,0 +1,20 @@
+package com.vinay.sample.service;
+
+public class HelperService {
+
+    public void addNumbers() {
+        int a = 10;
+        String b = "20";
+        int c = a + b;
+    }
+
+    public void calculateSum() {
+        int result = x + 10;
+    }
+
+    public int multiply(int a, int b) {
+        if (a > 0)
+            return a * b;
+    }
+
+}
