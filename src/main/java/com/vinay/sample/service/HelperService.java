b3d2153161aa8a38c77cdde95a784526881f2575
“acgawade” <gawade.achyut96@gmail.com>
2025-11-09 10:25:32 +0000

    Automated fix: build error resolution

--- src/main/java/com/vinay/sample/service/HelperService.java
+++ src/main/java/com/vinay/sample/service/HelperService.java
@@ -5,16 +5,19 @@ public class HelperService {
     public void addNumbers() {
         int a = 10;
         String b = "20";
-        int c = a + b;
+        int c = a + Integer.parseInt(b);
     }
 
     public void calculateSum() {
+        int x = 0;
         int result = x + 10;
     }
 
     public int multiply(int a, int b) {
         if (a > 0)
+        if (a > 0)
             return a * b;
+        else
+            return 0;
     }
-
 }
